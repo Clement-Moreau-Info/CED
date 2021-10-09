701c9b6975daf61eeb31e92ac4854222bc4ce3d1
Clement-Moreau-Info <88057976+Clement-Moreau-Info@users.noreply.github.com>
2021-10-09 11:53:46 +0200

    Update cxt_edit.py

--- cxt_edit.py
+++ cxt_edit.py
@@ -1,16 +1,25 @@
 from enum import Enum
 from typing import List
 
-
+##
+# Different edit operations
+##
 class Edit(Enum):
     MOD = 1
     ADD = 2
     DEL = 3
 
-
+##
+# Contextual Edit Operation
+#
+# op        : Edit operation performed
+# x         : New symbol edited
+# k_edit    : position of edition (in [1, len(S_i)]) 
+# S_i       : Sequence of symbols
+##
 class Cxt_edit:
-    def __init__(self, op: Edit, x: str, i_edit: int, S_i: List[str]):
+    def __init__(self, op: Edit, x: str, k_edit: int, S_i: List[str]):
         self.op = op
         self.x = x
-        self.i_edit = i_edit - 1
+        self.k_edit = k_edit - 1 # Begin at 0
         self.S_i = S_i
